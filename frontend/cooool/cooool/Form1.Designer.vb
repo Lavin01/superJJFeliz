0183bd9928ea8d9d49f0667b8f37e354c3ac8c54
Angel Lavin <bywarepro@gmail.com>
2022-06-13 04:53:55 -0500

    Coool agregado para la nueva maquetacion de VB.Net

---
+++ frontend/cooool/cooool/Form1.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.lstTicket = New System.Windows.Forms.ListBox()
+        Me.DataGridCool = New System.Windows.Forms.DataGridView()
+        Me.id = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.descripcion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.botonUno = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.btnCodigo = New System.Windows.Forms.Button()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridCool, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1220, 255)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Un Articulo"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1114, 172)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(304, 62)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "50 > Articulos"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'lstTicket
+        '
+        Me.lstTicket.FormattingEnabled = True
+        Me.lstTicket.Location = New System.Drawing.Point(18, 21)
+        Me.lstTicket.Name = "lstTicket"
+        Me.lstTicket.Size = New System.Drawing.Size(360, 693)
+        Me.lstTicket.TabIndex = 2
+        '
+        'DataGridCool
+        '
+        Me.DataGridCool.BackgroundColor = System.Drawing.Color.WhiteSmoke
+        Me.DataGridCool.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.DataGridCool.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridCool.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.id, Me.nombre, Me.descripcion, Me.botonUno})
+        Me.DataGridCool.Location = New System.Drawing.Point(991, 366)
+        Me.DataGridCool.Name = "DataGridCool"
+        Me.DataGridCool.Size = New System.Drawing.Size(541, 150)
+        Me.DataGridCool.TabIndex = 3
+        '
+        'id
+        '
+        Me.id.HeaderText = "Identificador"
+        Me.id.Name = "id"
+        Me.id.ReadOnly = True
+        '
+        'nombre
+        '
+        Me.nombre.HeaderText = "Nombre"
+        Me.nombre.Name = "nombre"
+        Me.nombre.ReadOnly = True
+        Me.nombre.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.nombre.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        '
+        'descripcion
+        '
+        Me.descripcion.HeaderText = "Descripcion"
+        Me.descripcion.Name = "descripcion"
+        Me.descripcion.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.descripcion.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        '
+        'botonUno
+        '
+        Me.botonUno.HeaderText = "Editar"
+        Me.botonUno.Name = "botonUno"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.PictureBox3.Image = Global.cooool.My.Resources.Resources.bg_white_bold
+        Me.PictureBox3.Location = New System.Drawing.Point(404, 12)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(398, 710)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 6
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Tag = ""
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.cooool.My.Resources.Resources.bg_500_cbold
+        Me.PictureBox1.Location = New System.Drawing.Point(0, -3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(398, 739)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Tag = ""
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.cooool.My.Resources.Resources.bg_500_bold
+        Me.PictureBox2.Location = New System.Drawing.Point(378, -3)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(886, 739)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Tag = ""
+        '
+        'btnCodigo
+        '
+        Me.btnCodigo.BackColor = System.Drawing.Color.White
+        Me.btnCodigo.FlatAppearance.BorderSize = 0
+        Me.btnCodigo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCodigo.Font = New System.Drawing.Font("Franklin Gothic Medium", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCodigo.ForeColor = System.Drawing.Color.Green
+        Me.btnCodigo.Image = Global.cooool.My.Resources.Resources.addX128
+        Me.btnCodigo.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.btnCodigo.Location = New System.Drawing.Point(486, 183)
+        Me.btnCodigo.Name = "btnCodigo"
+        Me.btnCodigo.Size = New System.Drawing.Size(235, 175)
+        Me.btnCodigo.TabIndex = 7
+        Me.btnCodigo.Text = "INGRESAR CODIGO"
+        Me.btnCodigo.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.btnCodigo.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage
+        Me.btnCodigo.UseVisualStyleBackColor = False
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Document = Me.PrintDocument1
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Font = New System.Drawing.Font("Franklin Gothic Medium", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(558, 58)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(83, 61)
+        Me.lblTotal.TabIndex = 8
+        Me.lblTotal.Text = "$0"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Franklin Gothic Medium", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(419, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(133, 61)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Total"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1430, 734)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.btnCodigo)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.DataGridCool)
+        Me.Controls.Add(Me.lstTicket)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridCool, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents lstTicket As ListBox
+    Friend WithEvents DataGridCool As DataGridView
+    Friend WithEvents id As DataGridViewTextBoxColumn
+    Friend WithEvents nombre As DataGridViewTextBoxColumn
+    Friend WithEvents descripcion As DataGridViewTextBoxColumn
+    Friend WithEvents botonUno As DataGridViewButtonColumn
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents btnCodigo As Button
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ frontend/cooool/cooool/Form1.vb
@@ -0,0 +1,151 @@
+﻿Imports Newtonsoft.Json
+
+Public Class Form1
+    Dim valorItems = ""
+    Dim valorPrecio As String = ""
+    Dim PrecioGeneral As Double = 0.0
+
+    Function LoadList() As String
+        lstTicket.Items.Clear()
+        lstTicket.Items.Add("                                         SUPER JJ FELIZ")
+        lstTicket.Items.Add("                                      Tel: 868-000-0000")
+        lstTicket.Items.Add("                                   RFC: GORDIS55CA1DC")
+        lstTicket.Items.Add("------------------------------------------------ TICKET -------------------------------------------------------")
+        Return 0
+    End Function
+    Function ApiUNProducto(ByVal codigo As String, ByVal tipo As String) As String
+        Dim api = New DBApi
+
+        Dim url = "http://btecvi.lavin.cool:3500/api/super/articulo?codigo=" + codigo
+        Dim headers = New List(Of Parametro)
+        Dim parametros = New List(Of Parametro)
+
+        Dim response = api.MGet(url, headers, parametros)
+
+        Dim objeto = JsonConvert.DeserializeObject(Of Articulo)(response)
+
+        If objeto.id Then
+            Select Case tipo
+                Case "nombre"
+                    valorPrecio += "," + objeto.precio
+                    valorItems += "," + objeto.nombre
+                    ActualizarDatos()
+                    Return objeto.nombre
+                Case Else
+                    MsgBox("Otro")
+            End Select
+        End If
+        Return 0
+    End Function
+
+    Function ActualizarDatos() As String
+        Dim testArray() As String = Split(valorItems, ",")
+        Dim testPrecio() As String = Split(valorPrecio, ",")
+
+        LoadList()
+
+        PrecioGeneral = 0.0
+
+        For i As Integer = 0 To testArray.Length - 1
+            If testArray(i) <> "" Then
+                lstTicket.Items.Add(testArray(i) + " - " + testPrecio(i))
+                Console.WriteLine(PrecioGeneral)
+
+                PrecioGeneral += Val(testPrecio(i))
+                lblTotal.Text = "$" + PrecioGeneral.ToString
+
+                Console.WriteLine(testArray(i))
+            End If
+        Next
+        Return 0
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lstTicket.Items.Add("                                         SUPER JJ FELIZ")
+        lstTicket.Items.Add("                                        Tel: 868-000-0000")
+        lstTicket.Items.Add("                                    RFC: GORDIS55CA1DC")
+        lstTicket.Items.Add("------------------------------------------------ TICKET -------------------------------------------------------")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim api = New DBApi
+
+        Dim url = "http://btecvi.lavin.cool:3500/api/super/articulo?codigo=do54h"
+        Dim headers = New List(Of Parametro)
+        Dim parametros = New List(Of Parametro)
+
+        Dim response = api.MGet(url, headers, parametros)
+
+        Dim objeto = JsonConvert.DeserializeObject(Of Articulo)(response)
+
+        MessageBox.Show("Articulo id: " + objeto.id + " // Nombre: " + objeto.nombre + " ----- Descripcion: " + objeto.descripcion)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim api = New DBApi
+
+        Dim url = "http://btecvi.lavin.cool:3500/api/super/articulos"
+        Dim headers = New List(Of Parametro)
+        Dim parametros = New List(Of Parametro)
+
+        Dim response = api.MGet(url, headers, parametros)
+        MsgBox(response(2))
+        Console.WriteLine(response)
+        Console.WriteLine("COOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL")
+
+        Console.WriteLine(response)
+        Console.WriteLine("COOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL")
+
+        MsgBox(response(2))
+        Dim objeto = JsonConvert.DeserializeObject(response)
+        Console.WriteLine(objeto(1).ToString)
+        Dim objeto2 = JsonConvert.DeserializeObject(Of Articulo)(objeto(1).ToString)
+        MessageBox.Show(objeto2.descripcion)
+        Console.WriteLine(objeto(1))
+        Dim listadoNumero = 0
+        ' objSL = El Objeto Seleccionado del Listado
+        lstTicket.Items.Clear()
+
+        While listadoNumero <= 50
+            Try
+                Dim objSL = JsonConvert.DeserializeObject(Of Articulo)(objeto(listadoNumero).ToString)
+                ' MessageBox.Show(objSL.descripcion)
+                lstTicket.Items.Add(objSL.descripcion)
+                DataGridCool.Rows.Add(New String() {objSL.id, objSL.descripcion, objSL.nombre, "Editar"})
+                ' DataGridCool.DataTable.AddRow(1, "John Doe", True)
+            Catch ex As Exception
+                Return
+            End Try
+            listadoNumero += 1
+        End While
+    End Sub
+
+    Private Sub ingresarCodigo_Manualmente(sender As Object, e As EventArgs) Handles btnCodigo.Click
+        Dim Message, Title, CDefault, MiCodigo
+        Message = "Ingresa el codigo que tiene el producto que deseas registrar"    ' Set prompt.
+        Title = "REGISTRAR PRODUCTO MANUALMENTE"    ' Set title.
+        CDefault = ""    ' Set default.
+        ' Display message, title, and default value.
+        MiCodigo = InputBox(Message, Title, CDefault)
+        If CStr(Len(MiCodigo)) > 0 Then
+            'MsgBox(ApiUNProducto(MiCodigo, "nombre"))
+            ApiUNProducto(MiCodigo, "nombre")
+            'MsgBox("Registrate el codigo: " & Chr(13) & Chr(10) & MiCodigo & Chr(13) & Chr(10) & Chr(13) & Chr(10) & "(MANZANA)")
+        End If
+    End Sub
+End Class
+
+Class Articulo
+    Public Property id As String
+    Public Property nombre As String
+    Public Property descripcion As String
+
+    Public Property cantidad As Double
+
+    Public Property unidad As Integer
+
+    Public Property codigo As String
+
+    Public Property seccion As Integer
+
+    Public Property precio As String
+End Class
